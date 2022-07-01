5b39081b0ed87c319601b546efcca7ac44db76e8
Kurc <kostins91@gmail.com>
2022-07-01 18:39:24 +0300

    fix

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+
 import java.sql.*;
 import java.util.*;
 
@@ -21,9 +22,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-        try {
-            Statement stmt = connection.createStatement();
-
+        try (Statement stmt = connection.createStatement()) {
             stmt.executeUpdate("CREATE TABLE `new_schema`.`User` (\n" +
                     "  `id` BIGINT NOT NULL auto_increment,\n" +
                     "  `name` VARCHAR(45) NOT NULL,\n" +
@@ -36,16 +35,14 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        try {
-            Statement stmt = connection.createStatement();
+        try (Statement stmt = connection.createStatement()) {
             stmt.executeUpdate("DROP TABLE `new_schema`.`User`;");
         } catch (SQLException e) {
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            PreparedStatement ptmt = connection.prepareStatement("INSERT INTO `new_schema`.`User` (name, lastName, age) values (?,?,?)");
+        try (PreparedStatement ptmt = connection.prepareStatement("INSERT INTO `new_schema`.`User` (name, lastName, age) values (?,?,?)")) {
             ptmt.setString(1, name);
             ptmt.setString(2, lastName);
             ptmt.setByte(3, age);
@@ -57,8 +54,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        try {
-            PreparedStatement ptmt = connection.prepareStatement("delete from `new_schema`.`User` where id = ?;");
+        try (PreparedStatement ptmt = connection.prepareStatement("delete from `new_schema`.`User` where id = ?;")){
             ptmt.setLong(1, id);
             ptmt.executeUpdate();
         } catch (SQLException e) {
@@ -69,13 +65,12 @@ public class UserDaoJDBCImpl implements UserDao {
     public List<User> getAllUsers() {
         List<User> list = new ArrayList<>();
 
-        try {
-            Statement stmt = connection.createStatement();
+        try (Statement stmt = connection.createStatement()){
             ResultSet rs = stmt.executeQuery("SELECT * FROM `new_schema`.`user`;");
             while (rs.next()) {
                 list.add(new User(rs.getString(2), rs.getString(3), rs.getByte(4)));
             }
-            for(User u : list){
+            for (User u : list) {
                 System.out.println(u.toString());
             }
         } catch (SQLException e) {
@@ -85,8 +80,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try {
-            Statement stmt = connection.createStatement();
+        try (Statement stmt = connection.createStatement()){
             stmt.executeUpdate("TRUNCATE new_schema.user;");
         } catch (SQLException e) {
         }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,14 +6,27 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
+
+    private static final String url = "jdbc:mysql://localhost:3306/mysql";
+    private static final String pass = "root";
+    private static final String user = "root";
+    private static Connection conn = null;
+
     public static Connection util() throws SQLException {
-        Connection conn = null;
-        String url = "jdbc:mysql://localhost:3306/mysql";
+
         Properties info = new Properties();
-        info.put("user", "root");
-        info.put("password", "root");
+        info.put("user", user);
+        info.put("password", pass);
 
         conn = DriverManager.getConnection(url, info);
         return conn;
+    }
+
+    public static void close(){
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
